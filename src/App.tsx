a5edb340b530319e8af27b779478b40a122c6c4a
somapujith <somapujith@gmail.com>
2025-12-31 03:13:46 +0530

    order management system table page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Dashboard from "./pages/dashboard"
+import Ordermanagement from "./pages/Ordermanagement"
 import Sidebar from "./components/sidebar/sidebar"
 const App = () => {
   return (
     <div>
       <div className="flex flex-row">
         <Sidebar />
-        <Dashboard />
+        <Ordermanagement />
       </div>
     </div>
 
--- src/pages/OrderManagement.tsx
+++ src/pages/OrderManagement.tsx
@@ -1,9 +1,13 @@
-const OrderManagement = () => {
+
+import { OrderTable } from '../components/Ordermanagementcomp';
+
+const Ordermanagement = () => {
   return (
-    <div className="p-4 bg-white rounded shadow min-h-screen ">
-      <h1 className="text-2xl font-bold mb-4">Order Management</h1>
+    <div className="p-4 bg-white rounded shadow min-h-screen">
+      <h1 className="text-2xl font-bold mb-6">Order Management</h1>
+      <OrderTable />
     </div>
   )
 }
 
-export default OrderManagement
+export default Ordermanagement
