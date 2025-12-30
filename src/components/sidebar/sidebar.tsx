da0b04c9c69b36cff56aa495c0a7faef2a76280a
Ajithprudhvi <ajithprudhvi128@gmail.com>
2025-12-31 02:41:20 +0530

    sidebar5

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -3,16 +3,19 @@
 const Sidebar = () => {
     return (
         <div className="w-80 h-screen flex flex-col border border-black">
-           
-           
+
+
             <div className="flex-1 flex flex-col">
                 <div className="p-2 flex items-center justify-center border-b border-black">
                     <img src="/src/assets/logo/eilogofull.svg" alt="Logo" className="max-h-10 max-w-full object-contain" />
                 </div>
 
 
-                <div className="bg-red-500 flex-1">
-                    first part - bottom
+                <div className="flex flex-col flex-1 p-2 items-center justify-center">
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>4</div>
                 </div>
             </div>
 
