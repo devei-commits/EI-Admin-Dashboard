cd357a996f73eecc7233d91338c3b6e7bd7065c8
somapujith <somapujith@gmail.com>
2025-12-31 03:20:18 +0530

    made changes

--- src/pages/Usermanagement.tsx
+++ src/pages/Usermanagement.tsx
@@ -1,1 +1,11 @@
-rafce
+
+
+const Usermanagement = () => {
+  return (
+    <div className="p-4 bg-white rounded shadow min-h-screen ">
+      <h1 className="text-2xl font-bold mb-4">User Management 1</h1>
+    </div>
+  )
+}
+
+export default Usermanagement
