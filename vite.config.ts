36b40f3d9d98fcc893cdddf53a7d4ada61c3f84a
SPR DLX Studios <hello@sprdlx.co>
2026-03-02 00:58:21 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -77,8 +77,8 @@ export default defineConfig({
     strictPort: true,
     proxy: {
       '/api': {
-        //target: 'https://ei-website-backend-production.up.railway.app/',
-        target: 'http://127.0.0.1:3000',
+        target: 'https://ei-website-backend-production.up.railway.app/',
+        //target: 'http://127.0.0.1:3000',
         changeOrigin: true,
         rewrite: (path) => path,
       },
